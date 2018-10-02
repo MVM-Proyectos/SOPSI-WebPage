0aa98dd061e6991b8be3876d98bb734513367f00
RodrigoMahalufRecasens <rmahaluf@gmail.com>
2018-10-02 17:13:48 -0300

    creada la aplicación SOPSIapp

--- SOPSI/SOPSI/settings.py
+++ SOPSI/SOPSI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'SOPSIapp.apps.SopsiappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
