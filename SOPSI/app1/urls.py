58660f11818b6f3c0dac77aaba912ade4e1fcc1f
LenadroMagga <leandromagga16@gmail.com>
2018-11-27 16:36:59 -0300

    changes

--- SOPSI/app1/urls.py
+++ SOPSI/app1/urls.py
@@ -2,15 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name='app1'
 urlpatterns=[
     path('', views.index, name='index'),
-
     #Path to DETAIL
     path('<int:question_id>/',views.detail, name='detail'),
     #Path to RESULTS
     path('<int:question_id>/results/',views.results, name='results'),
     #Path to VOTE
     path('<int:question_id>/vote/',views.vote, name='vote'),
-
 ]
 
--- SOPSI/app1/views.py
+++ SOPSI/app1/views.py
@@ -1,5 +1,5 @@
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 from django.http import Http404
@@ -7,18 +7,12 @@ from django.http import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by('pub_date')[:5]
-    #template = loader.get_template('app1/index.html')
     context= {'latest_question_list': latest_question_list,}
-    #return HttpResponse(template.render(context, request))
     return render(request, 'app1/index.html' ,context)
 
 def detail(request, question_id):
-    try:
-        question=Question.objects.get(pk=quiestion_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exists")
-
-    return render(request, 'polls/detail.html', {'question':question})
+    question=get_object_or_404(Question, pk=question_id)
+    return render(request, 'app1/detail.html', {'question':question})
 
 
 def results(request, question_id):
