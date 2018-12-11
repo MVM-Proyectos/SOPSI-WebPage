561ba4a4b836a7aaf67895bb5c74cd53a1c492e4
LenadroMagga <leandromagga16@gmail.com>
2018-12-11 16:25:26 -0300

    send

--- SOPSI/app1/urls.py
+++ SOPSI/app1/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
-
 from . import views
 
 app_name='app1'
 urlpatterns=[
-    path('', views.index, name='index'),
-    #Path to DETAIL
-    path('<int:question_id>/',views.detail, name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    #path to DETAIL
+    path('<int:pk>/',views.DetailView.as_view(), name='detail'),
     #Path to RESULTS
-    path('<int:question_id>/results/',views.results, name='results'),
+    path('<int:pk>/results/',views.ResultsView.as_view(), name='results'),
     #Path to VOTE
     path('<int:question_id>/vote/',views.vote, name='vote'),
 ]
